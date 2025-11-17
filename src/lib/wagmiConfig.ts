96cbfccbf8c4b475d7ed48d64abaf33299402b6a
khanmosajjid <mosajjid.khan@gmail.com>
2025-11-17 18:06:09 +0530

    update wallets

--- src/lib/wagmiConfig.ts
+++ src/lib/wagmiConfig.ts
@@ -1,5 +1,16 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import {
+    bitgetWallet,
+    coinbaseWallet,
+    ledgerWallet,
+    safepalWallet,
+    tokenPocketWallet,
+    trustWallet,
+    walletConnectWallet,
+    metaMaskWallet,
+    rainbowWallet,
+} from '@rainbow-me/rainbowkit/wallets';
+import {
     bsc,
     bscTestnet
 } from 'wagmi/chains';
@@ -9,5 +20,26 @@ export const config = getDefaultConfig({
     appName: 'ETHAN DeFi',
     projectId: '9c1d73388cbde2a0cebefd5ded95beaf',
     chains: [bsc, bscTestnet],
+    wallets: [
+        {
+            groupName: 'Popular',
+            wallets: [
+                metaMaskWallet,
+                rainbowWallet,
+                coinbaseWallet,
+                trustWallet,
+                walletConnectWallet,
+            ],
+        },
+        {
+            groupName: 'Hardware & Regionals',
+            wallets: [
+                ledgerWallet,
+                safepalWallet,
+                bitgetWallet,
+                tokenPocketWallet,
+            ],
+        },
+    ],
     ssr: false,
 });
