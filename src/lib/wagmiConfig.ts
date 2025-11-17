d6d55c9e09c213eaa29636f97e595049d476eaa3
khanmosajjid <mosajjid.khan@gmail.com>
2025-11-17 14:48:23 +0530

    undo

--- src/lib/wagmiConfig.ts
+++ src/lib/wagmiConfig.ts
@@ -1,28 +1,12 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { createConfig } from 'wagmi';
-import {  bsc} from 'wagmi/chains';
+import {  
+    bsc
+} from 'wagmi/chains';
 
-import { connectorsForWallets } from '@rainbow-me/rainbowkit';
-import {
-    rainbowWallet,
-    walletConnectWallet,
-    bitgetWallet
-} from '@rainbow-me/rainbowkit/wallets';
-const connectors = connectorsForWallets(
-    [
-        {
-            groupName: 'Recommended',
-            wallets: [rainbowWallet, walletConnectWallet,bitgetWallet],
-        },
-    ],
-    {
-        appName: 'ETHAN DeFi',
-        projectId: '9c1d73388cbde2a0cebefd5ded95beaf',
-        
-    }
-);
-export const config = createConfig({
-    connectors,
-    chains: [bsc],
+
+export const config = getDefaultConfig({
+    appName: 'ETHAN DeFi',
+    projectId: '9c1d73388cbde2a0cebefd5ded95beaf',
+    chains: [ bsc],
     ssr: false,
 });
